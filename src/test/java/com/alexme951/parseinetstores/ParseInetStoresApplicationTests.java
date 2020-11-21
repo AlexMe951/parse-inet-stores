13376f8523be38848555e6a7675894f43d7dfadb
aleksandr_kolinchenko@epam.com <aleksandr_kolinchenko@epam.com>
2020-11-21 07:42:02 +0300

    Initial commit from Spring Initialzr

---
+++ src/test/java/com/alexme951/parseinetstores/ParseInetStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexme951.parseinetstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseInetStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
