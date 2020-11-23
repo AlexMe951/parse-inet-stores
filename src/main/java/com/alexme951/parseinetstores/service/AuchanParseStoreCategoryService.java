3c55ec2ecbe3dce528c250da77a32ed6610faace
Sergey Ponomarev <ponser2000@gmail.com>
2020-11-23 16:40:56 +0300

    - Add overloaded method parseCategories(String subcategory) for parsing subcategory

--- src/main/java/com/alexme951/parseinetstores/service/AuchanParseStoreCategoryService.java
+++ src/main/java/com/alexme951/parseinetstores/service/AuchanParseStoreCategoryService.java
@@ -18,16 +18,32 @@ public class AuchanParseStoreCategoryService {
 
   @SneakyThrows
   public List<String> parseCategories() {
-    log.info("Start parsing");
+    log.info("================================= Start parsing =================================");
     Document result = Jsoup.parse(new URL(auchanInitialUrl), 10000);
     Elements allElements = result.body().getAllElements();
     String searchQuery = "[href^=/catalog/]";
     Elements selectedHrefs = allElements.select(searchQuery);
     List<String> selectedRefsUrl = selectedHrefs
-        .stream()
-        .map(elm -> elm.attributes().get("href"))
-        .collect(Collectors.toList());
+            .stream()
+            .map(elm -> elm.attributes().get("href"))
+            .collect(Collectors.toList());
     log.info(selectedRefsUrl.toString());
     return selectedRefsUrl;
   }
+
+  @SneakyThrows
+  public List<String> parseCategories(String subcategory) {
+    log.info("================================= Start parsing =================================");
+    Document result = Jsoup.parse(new URL(auchanInitialUrl), 10000);
+    Elements allElements = result.body().getAllElements();
+    String searchQuery = "[href^=/catalog/"+subcategory+"]";
+    Elements selectedHrefs = allElements.select(searchQuery);
+    List<String> selectedRefsUrl = selectedHrefs
+            .stream()
+            .map(elm -> elm.attributes().get("href"))
+            .collect(Collectors.toList());
+    log.info(selectedRefsUrl.toString());
+    return selectedRefsUrl;
+  }
+
 }
