d94718c9e995e4f84c4f5c335e07ed6d48440c4e
aleksandr_kolinchenko@epam.com <aleksandr_kolinchenko@epam.com>
2020-11-27 15:36:55 +0300

    Fix hibernate exception while work with real postgres DB
    - add properties for postgres and for hibernate to application.properties
    - changed id generation strategy
    - changed db dependancy from h2 to postgres

--- src/main/java/com/alexme951/parseinetstores/repository/dto/CatalogLink.java
+++ src/main/java/com/alexme951/parseinetstores/repository/dto/CatalogLink.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class CatalogLink {
 
   @Id
-  @GeneratedValue(strategy= GenerationType.AUTO)
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Long id;
   private LocalDateTime parsingTime;
   private String linkUrl;
