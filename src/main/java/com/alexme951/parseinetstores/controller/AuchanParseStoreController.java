29bf665118792deed9a4ce5467755dc210db21e9
Sergey Ponomarev <ponser2000@gmail.com>
2020-11-28 20:47:09 +0300

    - formatting code

--- src/main/java/com/alexme951/parseinetstores/controller/AuchanParseStoreController.java
+++ src/main/java/com/alexme951/parseinetstores/controller/AuchanParseStoreController.java
@@ -2,7 +2,9 @@ package com.alexme951.parseinetstores.controller;
 
 import com.alexme951.parseinetstores.repository.dto.CatalogLink;
 import com.alexme951.parseinetstores.service.AuchanParseStoreCategoryService;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,26 +19,27 @@ public class AuchanParseStoreController {
   private final AuchanParseStoreCategoryService service;
 
   @PostMapping("/parse/auchan/cataloglinks")
-  public ResponseEntity<List<String>> getAllCatalogLinks(){
+  public ResponseEntity<List<String>> getAllCatalogLinks() {
     List<String> result = service.parseAllCatalogLinks();
     return ResponseEntity.ok().body(result);
   }
 
   @PostMapping("/parse/auchan/cataloglinks/{category}")
   public ResponseEntity<List<String>> getAllCategoryCatalogLinks(
-      @PathVariable("category") String category){
+      @PathVariable("category") String category) {
     List<String> result = service.parseAllCategoryCatalogLinks(category);
     return ResponseEntity.ok().body(result);
   }
 
   @GetMapping("parse/auchan/cataloglinks")
-  public ResponseEntity<Iterable<CatalogLink>> getAllSavedCatalogLinks(){
+  public ResponseEntity<Iterable<CatalogLink>> getAllSavedCatalogLinks() {
     Iterable<CatalogLink> result = service.getAllSavedCatalogLinks();
     return ResponseEntity.ok().body(result);
   }
 
   @GetMapping("parse/auchan/cataloglinks/{linkUrl}")
-  public ResponseEntity<Iterable<CatalogLink>> getAllByLinkUrl(@PathVariable("linkUrl") String linkUrl){
+  public ResponseEntity<Iterable<CatalogLink>> getAllByLinkUrl(
+      @PathVariable("linkUrl") String linkUrl) {
     List<CatalogLink> result = service.findAllByLinkUrl(linkUrl);
     return ResponseEntity.ok().body(result);
   }
--- src/main/java/com/alexme951/parseinetstores/repository/dto/CatalogLink.java
+++ src/main/java/com/alexme951/parseinetstores/repository/dto/CatalogLink.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 public class CatalogLink {
 
   @Id
-  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private LocalDateTime parsingTime;
   private String linkUrl;
