6c17719bddd1015ab60506b8e9668bf9df2b891a
RomanKrasnodar <71653248+RomanKrasnodar@users.noreply.github.com>
2020-12-14 21:22:00 +0400

    Merge branch 'master' of https://github.com/AlexMe951/parse-inet-stores
    # Conflicts:
    #	src/main/java/com/alexme951/parseinetstores/controller/AuchanParseStoreController.java
    #	src/main/java/com/alexme951/parseinetstores/old/AuchanParseStoreService.java
    #	src/test/java/com/alexme951/parseinetstores/controller/AuchanParseStoreControllerTest.java
    #	utils/requests.http

--- src/main/java/com/alexme951/parseinetstores/service/db/impl/CategoryDatabaseSearchServiceImpl.java
+++ src/main/java/com/alexme951/parseinetstores/service/db/impl/CategoryDatabaseSearchServiceImpl.java
@@ -4,38 +4,99 @@ import com.alexme951.parseinetstores.repository.dto.parsing.CategoryLinkParsingD
 import com.alexme951.parseinetstores.repository.dto.parsing.CategoryParsingDto;
 import com.alexme951.parseinetstores.service.db.CategoryDatabaseSearchService;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CategoryDatabaseSearchServiceImpl implements CategoryDatabaseSearchService {
+  SessionFactory factory;
+  Session session;
+
 
   @Override
   public List<CategoryLinkParsingDto> readAllCategoryLinks() {
-    return null;
+    createFactoryCategoryLinkParsingDto();
+    List<CategoryLinkParsingDto> allCategoryLinksList = session
+        .createQuery("SELECT '*' from CategoryLinkParsingDto").getResultList();
+    session.getTransaction().commit();
+    factory.close();
+    return allCategoryLinksList;
   }
+
 
   @Override
   public List<CategoryLinkParsingDto> readSubCategoryLinksByCategory(String category) {
-    return null;
+    createFactoryCategoryLinkParsingDto();
+    List<CategoryLinkParsingDto> allCategoryLinksList = session
+        .createQuery("select '*' from CategoryLinkParsingDto where linkUrl like :category")
+        .setParameter("category", "%" + category + "%").getResultList();
+    session.getTransaction().commit();
+    factory.close();
+    return allCategoryLinksList;
   }
 
   @Override
   public List<CategoryParsingDto> readAllCategories() {
-    return null;
+    createFactoryCategoryParsingDto();
+    List<CategoryParsingDto> allCategoriesList = session
+        .createQuery("select distinct '*' from CategoryParsingDto").getResultList();
+    session.getTransaction().commit();
+    factory.close();
+    return allCategoriesList;
   }
 
   @Override
   public List<CategoryParsingDto> readSubCategoriesByCategory(String category) {
-    return null;
+    createFactoryCategoryParsingDto();
+    List<CategoryParsingDto> allSubCategoriesByCategoryList = session
+        .createQuery("select '*' from CategoryParsingDto where name like :category")
+        .setParameter("category", "%" + category + "%").getResultList();
+    session.getTransaction().commit();
+    factory.close();
+    return allSubCategoriesByCategoryList;
+
   }
 
   @Override
   public CategoryParsingDto readCategoryByName(String categoryName) {
-    return null;
+    createFactoryCategoryParsingDto();
+    CategoryParsingDto categoryParsingDtoByName = (CategoryParsingDto) session
+        .createQuery("select distinct '*' from CategoryParsingDto where name = :categoryName")
+        .setParameter("categoryName", "%" + categoryName + "%" ).getSingleResult();
+    session.getTransaction().commit();
+    factory.close();
+    return categoryParsingDtoByName;
   }
 
   @Override
   public CategoryParsingDto readCategoryById(Long categoryId) {
-    return null;
+    createFactoryCategoryParsingDto();
+    CategoryParsingDto categoryParsingDtoByName = (CategoryParsingDto) session
+        .createQuery("select distinct '*' from CategoryParsingDto where id = :categoryId")
+        .setParameter("categoryId", categoryId).getSingleResult();
+    session.getTransaction().commit();
+    factory.close();
+    return categoryParsingDtoByName;
+  }
+
+  public void createFactoryCategoryLinkParsingDto(){
+    factory = new Configuration()
+        .configure("hibernate.properties")
+        .addAnnotatedClass(CategoryLinkParsingDto.class)
+        .buildSessionFactory();
+    session = factory.getCurrentSession();
+    session.beginTransaction();
+  }
+
+  public void createFactoryCategoryParsingDto(){
+    factory = new Configuration()
+        .configure("hibernate.properties")
+        .addAnnotatedClass(CategoryParsingDto.class)
+        .buildSessionFactory();
+    session = factory.getCurrentSession();
+    session.beginTransaction();
   }
 }
