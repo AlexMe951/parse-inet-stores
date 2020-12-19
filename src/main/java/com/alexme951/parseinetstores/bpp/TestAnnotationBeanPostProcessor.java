8df0c90e3f4bab5b9a8f352ec08470538c29b9dc
aleksandr_kolinchenko@epam.com <aleksandr_kolinchenko@epam.com>
2020-12-19 23:10:09 +0300

    Fixes and homework

--- src/main/java/com/alexme951/parseinetstores/bpp/TestAnnotationBeanPostProcessor.java
+++ src/main/java/com/alexme951/parseinetstores/bpp/TestAnnotationBeanPostProcessor.java
@@ -12,13 +12,16 @@ import org.springframework.util.ReflectionUtils;
 @Slf4j
 public class TestAnnotationBeanPostProcessor implements BeanPostProcessor {
 
-  @SneakyThrows
   @Override
   public Object postProcessBeforeInitialization(Object bean, String beanName) {
     TestAnnotation annotation = AnnotationUtils.findAnnotation(bean.getClass(), TestAnnotation.class);
     if (annotation != null){
-      log.info("Bean with @TestAnnotation found: {}", beanName);
-      log.info("Annotation value: {}", annotation.value());
+      Field field = ReflectionUtils.findField(bean.getClass(), "value");
+      if (field != null){
+        log.info("field = {}", field);
+        field.setAccessible(true);
+        ReflectionUtils.setField(field, bean, annotation.value());
+      }
     }
     return bean;
   }
@@ -28,12 +31,8 @@ public class TestAnnotationBeanPostProcessor implements BeanPostProcessor {
   public Object postProcessAfterInitialization(Object bean, String beanName) {
     TestAnnotation annotation = AnnotationUtils.findAnnotation(bean.getClass(), TestAnnotation.class);
     if (annotation != null){
-      Field field = ReflectionUtils.findField(bean.getClass(), "value");
-      if (field != null){
-        log.info("field = {}", field);
-        field.setAccessible(true);
-        field.set(bean, annotation.value());
-      }
+      log.info("Bean with @TestAnnotation found: {}", beanName);
+      log.info("Annotation value: {}", annotation.value());
     }
     return bean;
   }
