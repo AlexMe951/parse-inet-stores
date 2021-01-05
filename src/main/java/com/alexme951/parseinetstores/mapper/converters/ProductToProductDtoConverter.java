d74a7a6a8bc78071ff8b19cf2a0b01577d1eb5be
aleksandr_kolinchenko@epam.com <aleksandr_kolinchenko@epam.com>
2021-01-05 20:46:03 +0300

    ProductToProductDtoConverter implementation
    - gson dependency
    - converter implementation
    - test fixes

--- src/main/java/com/alexme951/parseinetstores/mapper/converters/ProductToProductDtoConverter.java
+++ src/main/java/com/alexme951/parseinetstores/mapper/converters/ProductToProductDtoConverter.java
@@ -2,24 +2,55 @@ package com.alexme951.parseinetstores.mapper.converters;
 
 import com.alexme951.parseinetstores.repository.dto.parsing.ProductParsingDto;
 import com.alexme951.parseinetstores.service.dto.Product;
+import com.google.gson.Gson;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.metadata.Type;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ProductToProductDtoConverter extends
     BidirectionalConverter<Product, ProductParsingDto> {
 
+  private final Gson gson;
+
   @Override
-  public ProductParsingDto convertTo(Product source, Type<ProductParsingDto> destinationType,
+  public ProductParsingDto convertTo(Product source,
+      Type<ProductParsingDto> destinationType,
       MappingContext mappingContext) {
-    return null;
+    ProductParsingDto result = new ProductParsingDto();
+    result.setName(source.name());
+    result.setDescription(source.description());
+    result.setCode(source.code());
+    String attributes = gson.toJson(source.attributes());
+    result.setAttributes(attributes);
+    return result;
   }
 
   @Override
   public Product convertFrom(ProductParsingDto source, Type<Product> destinationType,
       MappingContext mappingContext) {
-    return null;
+
+    Map<Object, Object> parsedMap = gson.fromJson(source.getAttributes(), Map.class);
+
+    Map<String, String> attributes = parsedMap
+        .entrySet()
+        .stream()
+        .collect(
+            Collectors.toMap(
+                entry -> entry.getKey().toString(),
+                entry -> entry.getValue().toString(),
+                (first, second) -> first));
+
+    return new Product(
+        source.getName(),
+        source.getDescription(),
+        source.getCode(),
+        attributes
+    );
   }
 }
